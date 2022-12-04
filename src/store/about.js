f6e67b4e54faa71ccbed4e4100eacb8e94bd872c
FriendShip-90 <eoon@naver.com>
2022-12-04 18:14:55 +0900

    first commit

---
+++ src/store/about.js
@@ -0,0 +1,12 @@
+export default {
+    namespaced: true,
+    state: () => {
+        return {
+            name: "Man_Jock",
+            email: 'akstns921@gmail.com',
+            blog: 'https://heropy.blog',
+            phone: '+82-01-1234-5678',
+            image: 'https://heropy.blog/css/images/logo.png'
+        }
+    }
+}
